80d0eae3ece20c2fcb8fb4ced75cbcd35b27f13a
Daniel <daanilo2497@gmail.com>
2023-04-12 01:06:59 -0300

    Fix browser refresh when navigating

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,19 @@
 import { Nav, Container } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar'
 import Brand from '@components/Brand';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header className='bg-dark_blue--primary sticky-top shadow'>
             <Navbar className='py-2 navbar-fiufit'>
                 <Container className='px-5' fluid>
-                    <Navbar.Brand href='/'>
+                    <Navbar.Brand as={Link} to ='/'>
                         <Brand />
                     </Navbar.Brand>
                     <Nav>
                         <Nav.Item>
-                            <Nav.Link className='py-2' href='/admins'>
+                            <Nav.Link className='py-2' as={Link} to ='/admins'>
                                 <div className='text-white--primary d-flex align-items-center'>
                                     <i className='bi bi-gear pe-2 fs-4'></i>
                                     <span className='fs-4'>BACK OFFICE</span>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,28 @@
 import { Nav } from 'react-bootstrap';
 import { FaEye, FaUser, FaRegChartBar, FaServer, FaBolt } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <>
             <Nav activeKey={location.pathname} className="d-flex flex-column">
-                <Nav.Link href="/admins" id='management-navbar-first-link'>
+                <Nav.Link as={Link} to="/admins" id='management-navbar-first-link'>
                     <FaEye className='management-navbar-icon' /> 
                     <span>Administradores</span>
                 </Nav.Link>
-                <Nav.Link href="/users">
+                <Nav.Link as={Link} to="/users">
                     <FaUser className='management-navbar-icon' />
                     <span>Usuarios</span>
                 </Nav.Link>
-                <Nav.Link href="/trainings">
+                <Nav.Link as={Link} to="/trainings">
                     <FaBolt className='management-navbar-icon' />
                     <span>Entrenamientos</span>
                 </Nav.Link>
-                <Nav.Link href="/services">
+                <Nav.Link as={Link} to="/services">
                     <FaServer className='management-navbar-icon' />
                     <span>Servicios</span>
                 </Nav.Link>
-                <Nav.Link href="/metrics" id='management-navbar-last-link'>
+                <Nav.Link as={Link} to="/metrics" id='management-navbar-last-link'>
                     <FaRegChartBar className='management-navbar-icon' />
                     <span>Métricas</span>
                 </Nav.Link>
