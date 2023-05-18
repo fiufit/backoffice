c89413ab5dddb1f14aaf2aa02bfd847f23900edb
M. Iñaki Otegui <mati.otegui@gmail.com>
2023-05-17 22:03:27 -0300

    utils cleaning moving toLocalDateString

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,7 @@ export function delay(ms: number): Promise<void> {
 export function toLocalTimeString(date: string, locales: Intl.LocalesArgument, options: Intl.DateTimeFormatOptions): string {
     return new Date(date).toLocaleTimeString(locales, options);
 }
+
+export function toLocalDateString(date: string, locales: Intl.LocalesArgument, options: Intl.DateTimeFormatOptions) {
+    return new Date(date).toLocaleDateString(locales, options);
+}
