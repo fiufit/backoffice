ec6524bf5c5993497643a1eeb5740d1b2beb3e0e
M. Iñaki Otegui <mati.otegui@gmail.com>
2023-06-09 15:11:44 -0300

    min change how metrics are shown

--- src/components/MetricsUsers.tsx
+++ src/components/MetricsUsers.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 
 export default function MetricsUsers() {
 
+    const [showMetricsByInterval, setShowMetricsByInterval] = useState(false);
     const [showMetricsNewUsers, setShowMetricsNewUsers] = useState(false);
     const [showMetricsLogin, setShowMetricsLogin] = useState(false);
     const [showMetricsBlocked, setShowMetricsBlocked] = useState(false);
@@ -17,14 +18,14 @@ export default function MetricsUsers() {
 
     return (
         <div className="metrics-accordion">
-            <Accordion className='pt-4' defaultActiveKey={["metrics-users"]}>
+            <Accordion className='pt-4'>
                 <Accordion.Item eventKey="metrics-users">
                     <Accordion.Header className="metrics-item">Usuarios</Accordion.Header>
                     <Accordion.Body className="pb-4">
-                        <Accordion defaultActiveKey={["metrics-by-interval"]} alwaysOpen>
+                        <Accordion alwaysOpen>
                             <Accordion.Item eventKey="metrics-by-interval">
-                                <Accordion.Header>Búsqueda por día/intervalo</Accordion.Header>
-                                <Accordion.Body><MetricsByInterval /></Accordion.Body>
+                                <Accordion.Header onClick={() => {setShowMetricsByInterval(!showMetricsByInterval)}}>Búsqueda por día/intervalo</Accordion.Header>
+                                <Accordion.Body>{ showMetricsByInterval ? <MetricsByInterval /> : <></> }</Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item eventKey="metrics-new-users">
                                 <Accordion.Header onClick={() => {setShowMetricsNewUsers(!showMetricsNewUsers)}}>Nuevos usuarios</Accordion.Header>
